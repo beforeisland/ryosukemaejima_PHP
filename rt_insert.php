d7f41396b8386f342ad6ed7b9288f7bdc10a40cc
beforeisland <beforeisland@gmail.com>
2020-10-13 22:56:21 +0900

    update: いいね・リツイートの追加機能用に渡すリクエストの検査機能を追加
    セキュリティ対策にて、URLパラメータにてリクエストで渡す値が数値かどうかを判定するための処理及びエラーメッセージを追加

--- rt_insert.php
+++ rt_insert.php
@@ -3,19 +3,31 @@ session_start();
 require('dbconnect.php');
 
 //if:セッションに数値形式で値が保存されているか調査 
-if (is_numeric($_SESSION['id'])) {
-    //リツイートを追加する
-    $rtCopys = $db->prepare('SELECT * FROM posts WHERE id=?');
-    $rtCopys->execute(array($_REQUEST['post_id']));
-	$rtCopy = $rtCopys->fetch();
+if(is_numeric($_SESSION['id'])) {
 
-    $insert = $db->prepare('INSERT INTO posts SET message=?, member_id=?, rt_post_id=?, rt_member_id=?, created=NOW()');
-    $insert->execute(array(
-        $rtCopy['message'],
-        $rtCopy['member_id'],
-        $_REQUEST['post_id'],
-        $_SESSION['id']
-    ));
+    //if:リクエストにて受け取ったURLパラメータが数値形式で存在しているか調査
+    if(is_numeric($_REQUEST['post_id'])) {
+        //リツイートを追加する
+        $rtCopys = $db->prepare('SELECT * FROM posts WHERE id=?');
+        $rtCopys->execute(array($_REQUEST['post_id']));
+        $rtCopy = $rtCopys->fetch();
+
+        $insert = $db->prepare('INSERT INTO posts SET message=?, member_id=?, rt_post_id=?, rt_member_id=?, created=NOW()');
+        $insert->execute(array(
+            $rtCopy['message'],
+            $rtCopy['member_id'],
+            $_REQUEST['post_id'],
+            $_SESSION['id']
+        ));
+
+    } else {
+        echo 'error:URLパラメータが不正です';
+        exit();
+    }
+
+} else {
+    echo 'error:セッションにidが保存されていません';
+    exit();
 }
 
 header('Location: index.php');
