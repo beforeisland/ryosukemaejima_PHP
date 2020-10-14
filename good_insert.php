b668a23de40a8b59779c3902b5d48a0f314592e9
beforeisland <beforeisland@gmail.com>
2020-10-15 00:37:21 +0900

    add: いいね追加機能にいいね対象投稿の存在有無の検知機能を追加
    バリデーション対策にて機能追加。SQLで投稿を検索し、issetで変数がセットされているか調べる。

--- good_insert.php
+++ good_insert.php
@@ -2,17 +2,29 @@
 session_start();
 require('dbconnect.php');
 
+//SQL:いいね対象の投稿が存在するか探し出す
+$postSearches = $db->prepare('SELECT * FROM posts WHERE id=?');
+$postSearches->execute(array($_REQUEST['post_id']));
+$postSearch = $postSearches->fetch();
+
 //if:セッションに数値形式で値が保存されているか調査 
 if (is_numeric($_SESSION['id'])) {
 
     //if:リクエストにて受け取ったURLパラメータが数値形式で存在しているか調査
     if (is_numeric($_REQUEST['post_id'])) {
-        //いいねを追加する
-        $insert = $db->prepare('INSERT INTO goods SET member_id=?, post_id=?, created=NOW()');
-        $insert->execute(array(
-            $_SESSION['id'],
-            $_REQUEST['post_id']
-        ));
+
+        //if:いいね対象の投稿が存在すればいいね追加
+        if (isset($postSearch['id'])) {
+            //いいねを追加する
+            $insert = $db->prepare('INSERT INTO goods SET member_id=?, post_id=?, created=NOW()');
+            $insert->execute(array(
+                $_SESSION['id'],
+                $_REQUEST['post_id']
+            ));
+        } else {
+            echo 'error:いいね対象の投稿が存在しません';
+            exit();
+        }
     } else {
         echo 'error:URLパラメータが不正です';
         exit();
