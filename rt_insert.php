a25a0f3ff28b4d56ef83ea84e725ff0906c1969a
beforeisland <beforeisland@gmail.com>
2020-10-15 00:53:39 +0900

    add: リツイート追加機能にリツイート対象投稿の存在有無の検知機能を追加
    バリデーション対策にて機能追加。SQLで投稿を検索し、issetで変数がセットされているか調べる。

--- rt_insert.php
+++ rt_insert.php
@@ -2,23 +2,31 @@
 session_start();
 require('dbconnect.php');
 
+////SQL:リツイート対象の投稿が存在するか探し出す
+$rtSearches = $db->prepare('SELECT * FROM posts WHERE id=?');
+$rtSearches->execute(array($_REQUEST['post_id']));
+$rtSearch = $rtSearches->fetch();
+
 //if:セッションに数値形式で値が保存されているか調査 
 if (is_numeric($_SESSION['id'])) {
 
     //if:リクエストにて受け取ったURLパラメータが数値形式で存在しているか調査
     if (is_numeric($_REQUEST['post_id'])) {
-        //リツイートを追加する
-        $rtCopys = $db->prepare('SELECT * FROM posts WHERE id=?');
-        $rtCopys->execute(array($_REQUEST['post_id']));
-        $rtCopy = $rtCopys->fetch();
 
-        $insert = $db->prepare('INSERT INTO posts SET message=?, member_id=?, rt_post_id=?, rt_member_id=?, created=NOW()');
-        $insert->execute(array(
-            $rtCopy['message'],
-            $rtCopy['member_id'],
-            $_REQUEST['post_id'],
-            $_SESSION['id']
-        ));
+        //if:リツイート対象の投稿が存在すればいいね追加
+        if (isset($rtSearch['id'])) {
+            //リツイートを追加する
+            $insert = $db->prepare('INSERT INTO posts SET message=?, member_id=?, rt_post_id=?, rt_member_id=?, created=NOW()');
+            $insert->execute(array(
+                $rtSearch['message'],
+                $rtSearch['member_id'],
+                $_REQUEST['post_id'],
+                $_SESSION['id']
+            ));
+        } else {
+            echo 'error:リツイート元投稿が存在しません';
+            exit();
+        }
     } else {
         echo 'error:URLパラメータが不正です';
         exit();
